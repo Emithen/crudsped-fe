51b8d41c5efc53afa05f238dbc618b62e5bcc4f8
Emithen <86219540+Emithen@users.noreply.github.com>
2026-02-12 02:11:18 +0900

    feat: logout api connection

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,3 +1,24 @@
+import { logout } from "../api/auth";
+
 export const DashboardPage = () => {
-  return <div>Dashboard Page</div>;
+  const handleLogout = async () => {
+    try {
+      await logout();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  return (
+    <div className="flex h-screen w-full flex-col items-center">
+      <div className="mt-10 flex w-full items-center justify-between px-10">
+        <div className="text-2xl font-bold">Dashboard</div>
+        <button
+          className="cursor-pointer rounded-lg bg-gray-800 px-3 py-1 text-white hover:bg-gray-500"
+          onClick={handleLogout}
+        >
+          Logout
+        </button>
+      </div>
+    </div>
+  );
 };
