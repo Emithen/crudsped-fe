1f1424deea1fba3a57b86908beed46f9a10c69dc
Emithen <86219540+Emithen@users.noreply.github.com>
2026-02-12 02:16:27 +0900

    feat: getPosts test

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,6 +1,10 @@
+import { useEffect, useState } from "react";
 import { logout } from "../api/auth";
+import { getPosts, type Post } from "../api/post.api";
 
 export const DashboardPage = () => {
+  const [posts, setPosts] = useState<Post[]>([]);
+
   const handleLogout = async () => {
     try {
       await logout();
@@ -8,6 +12,26 @@ export const DashboardPage = () => {
       console.error(error);
     }
   };
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const data = await getPosts();
+        setPosts(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    try {
+      fetchPosts();
+    } catch (error) {
+      console.error(error);
+    }
+  }, []);
+
+  if (!posts) return null;
+
   return (
     <div className="flex h-screen w-full flex-col items-center">
       <div className="mt-10 flex w-full items-center justify-between px-10">
@@ -19,6 +43,33 @@ export const DashboardPage = () => {
           Logout
         </button>
       </div>
+
+      <div className="mt-10 flex w-full flex-col items-center gap-4">
+        {posts.map((post) => (
+          <div
+            key={post.id}
+            className="flex flex-col rounded-md border border-gray-300 p-4"
+          >
+            <div className="text-lg font-bold">{post.title}</div>
+            <div className="mt-2">{post.content}</div>
+            <div className="mt-2 text-sm text-gray-500">
+              작성일시: {formatDate(post.created_at)}
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
+};
+
+const formatDate = (dateString: string) => {
+  const date = new Date(dateString);
+  return new Intl.DateTimeFormat("ko-KR", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: false, // 24시간 형식
+  }).format(date);
 };
