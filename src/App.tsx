a70eae47c9ed81450ea40ee8e5235cbbb86f30df
Emithen <86219540+Emithen@users.noreply.github.com>
2026-02-11 21:58:18 +0900

    feat: posts api call test

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,36 @@
+import { useEffect, useState } from "react";
+import { getPosts, type Post } from "./api/post.api";
+
 // 루트 컴포넌트
 function App() {
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const data = await getPosts();
+      setPosts(data);
+    };
+
+    try {
+      fetchPosts();
+    } catch (error) {
+      console.error(error);
+    }
+  }, []);
+
+  if (!posts) return null;
+
   return (
     <>
       <div>App Component</div>
+      {posts.map((post) => (
+        <div key={post.id}>
+          <div>{post.title}</div>
+          <div>{post.author_id}</div>
+          <div>{post.content}</div>
+          <div>{post.created_at}</div>
+        </div>
+      ))}
     </>
   );
 }
---
+++ src/api/post.api.ts
@@ -0,0 +1,19 @@
+import { instance } from "./axios";
+
+export const getPosts = async () : Promise<Post[]> => {
+  try {
+    const res = await instance.get("/posts");
+    return res.data;
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+}
+
+export type Post = {
+  id: string;
+  author_id: string;
+  title: string;
+  content: string;
+  created_at: string;
+}
